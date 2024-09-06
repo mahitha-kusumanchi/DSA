98f64671929ea65b3c6d110928cb4dbfb8de8963
mahitha-kusumanchi <167613798+mahitha-kusumanchi@users.noreply.github.com>
2024-09-06 21:08:43 +0530

    Create preOrderTraversal.py

---
+++ preOrderTraversal.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+def preOrderTraversal(node):
+    li=[]
+    def preOrder(node):
+        li.append(node.data)
+        if node.left:
+            preOrder(node.left)
+        if node.right:
+            preOrder(node.right)
+    if node:
+        preOrder(node)
+    return li
+if __name__=="__main__":
+    root=TreeNode(1)
+    root.left=TreeNode(2)
+    root.right=TreeNode(3)
+    root.left.left=TreeNode(4)
+    root.left.right=TreeNode(5)
+    root.right.left=TreeNode(6)
+    root.right.right=TreeNode(7)
+    print(preOrderTraversal(root))
