321fdc0de64fee4836b81018f46ae7cd431a5273
mahitha-kusumanchi <167613798+mahitha-kusumanchi@users.noreply.github.com>
2024-09-06 21:14:22 +0530

    Update preOrderTraversal.py

--- preOrderTraversal.py
+++ preOrderTraversal.py
@@ -14,12 +14,4 @@ def preOrderTraversal(node):
     if node:
         preOrder(node)
     return li
-if __name__=="__main__":
-    root=TreeNode(1)
-    root.left=TreeNode(2)
-    root.right=TreeNode(3)
-    root.left.left=TreeNode(4)
-    root.left.right=TreeNode(5)
-    root.right.left=TreeNode(6)
-    root.right.right=TreeNode(7)
-    print(preOrderTraversal(root))
+
