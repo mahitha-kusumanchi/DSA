459258d4718bffd6f17d2da7ad708d5562943fc1
mahitha-kusumanchi <167613798+mahitha-kusumanchi@users.noreply.github.com>
2024-09-13 19:19:57 +0530

    Update BinaryTree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,41 +4,157 @@ class TreeNode:
         self.value=value
         self.left=None
         self.right=None
-def buildTree(values):
-    if not values:
-        return None
-    root = TreeNode(values[0])
-    q=deque([root])
-    i=1
-    while i<len(values):
-        current=q.popleft()
-        if i<len(values) and values[i] is not None:
-            current.left=TreeNode(values[i])
-            q.append(current.left)
-        i+=1
-        if i<len(values) and values[i] is not None:
-            current.right=TreeNode(values[i])
-            q.append(current.right)
-        i+=1
-    return root
-def print_level_order(root):
-    if not root:
+class BinaryTree:
+    def buildTree(self,values):
+        if values[0] is None:
+            return None
+        root=TreeNode(values[0])
+        q=deque([root])
+        i=1
+        while i<len(values):
+            temp=q.popleft()
+            if i<len(values) and values[i] is not None:
+                temp.left=TreeNode(values[i])
+                q.append(temp.left)
+            i+=1
+            if i<len(values) and values[i] is not None:
+                temp.right=TreeNode(values[i])
+                q.append(temp.right)
+            i+=1
+        return root
+    def preorder(self,root):
+        if not root:
+            return
+        print(root.value,end=" ")
+        self.preorder(root.left)
+        self.preorder(root.right)
         return
-    q=deque([root])
-    while q:
-        node=q.popleft()
-        print(node.value,end=" ")
-        if node.left:
-            q.append(node.left)
-        if node.right:
-            q.append(node.right)
-_values=input("Enter tree node values separated by space (use None for null nodes): ").split()
-values=[]
-for x in _values:
-    if x!='None':
-        values.append(int(x))
-    else:
-        values.append(None)
-root=buildTree(values)
-print("Level-order traversal of the tree:")
-print_level_order(root)
+    def postorder(self,root):
+        if not root:
+            return
+        self.postorder(root.left)
+        self.postorder(root.right)
+        print(root.value, end=" ")
+    def inorder(self,root):
+        if not root:
+            return
+        self.inorder(root.left)
+        print(root.value, end=" ")
+        self.inorder(root.right)
+    def depthFirst(self,root):
+        if not root:
+            return
+        stack=deque([root])
+        while stack:
+            temp=stack.pop()
+            print(temp.value,end=" ")
+            if temp.right:
+                stack.append(temp.right)
+            if temp.left:
+                stack.append(temp.left)
+    def size(self,root):
+        if not root:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        return 1+self.size(root.left)+self.size(root.right)
+    def height(self,root):
+        if not root:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        return 1+max(self.height(root.left),self.height(root.right))
+    def countLeaves(self,root):
+        if not root :
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        return self.countLeaves(root.left)+self.countLeaves(root.right)
+    def level_order_helper(self, root):
+        if not root:
+            return []
+        result = []
+        q = deque([root])
+        while q:
+            temp = q.popleft()
+            result.append(temp.value)
+            if temp.left:
+                q.append(temp.left)
+            if temp.right:
+                q.append(temp.right)
+        return result
+    def print_level_order(self, root):
+        values = self.level_order_helper(root)
+        print(" ".join(map(str, values)))
+    def clone(self, root):
+        if not root:
+            return None
+        new_root = TreeNode(root.value)
+        new_root.left = self.clone(root.left)
+        new_root.right = self.clone(root.right)
+        return new_root
+    def printMirror(self, root):
+        cloned_root = self.clone(root)
+        self.mirror(cloned_root)
+        values = self.level_order_helper(cloned_root)
+        print(" ".join(map(str, values)))
+    def mirror(self, root):
+        if not root:
+            return
+        root.left, root.right = root.right, root.left
+        self.mirror(root.left)
+        self.mirror(root.right)
+    def isFullTree(self,root):
+        if not root:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        if root.left and root.right:
+            return self.isFullTree(root.left) and self.isFullTree(root.right)
+        return False
+
+    def findAncestors(self, root, target):
+        if root is None:
+            return False
+        if root.value == target:
+            return True
+        if self.findAncestors(root.left, target) or self.findAncestors(root.right, target):
+            print(root.value, end=" ")
+            return True
+        return False
+if __name__=="__main__":
+    bt=BinaryTree()
+    _values=input().split()
+    values=[]
+    for x in _values:
+        if x != 'None':
+            values.append(int(x))
+        else:
+            values.append(None)
+    root=bt.buildTree(values)
+    print("Level Order traversal :",end=" ")
+    bt.print_level_order(root)
+    print("preorder traversal :",end=" ")
+    bt.preorder(root)
+    print()
+    print("postorder traversal :",end=" ")
+    bt.postorder(root)
+    print()
+    print("inorder traversal :",end=" ")
+    bt.inorder(root)
+    print()
+    print("depth first traversal :",end=" ")
+    bt.depthFirst(root)
+    print()
+    print("size :",end=" ")
+    print(bt.size(root))
+    print("height ",end=" ")
+    print(bt.height(root))
+    print("number of leaves :",end=" ",)
+    print(bt.countLeaves(root))
+    print("is full tree :",end=" ")
+    print(bt.isFullTree(root))
+    print("Mirror Tree:", end=" ")
+    print(bt.printMirror(root))
+    print("Ancestors :",end=" ")
+    print(bt.findAncestors(root,5))
